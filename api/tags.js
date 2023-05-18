74a9ed316aa3936aa3cab0f6470ddcd826bbe028
Joey Pagaduan <joey.paga220@gmail.com>
2023-05-18 10:21:15 -0700

    Server DAY 3: Wrote 'GET /api/tags/:tagName/posts'

--- api/tags.js
+++ api/tags.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
+const { getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
@@ -16,5 +17,20 @@ tagsRouter.get('/', async (req, res) => {
         posts
     });
 });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params;
+
+    try {
+        // use our method to get posts by tag name from the db
+        const posts = await getPostsByTagName(tagName);
+        // send out an object to the client { posts: // the posts }
+        res.send({
+            posts
+        });
+    }   catch ({ name, message }) {
+        next({ name, message });
+    }
+  });
 
 module.exports = tagsRouter;
